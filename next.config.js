6a429581a9288223022c106d8e16f5d93bd44d0d
Sudhir Yadav <sudhirkyadav51@gmail.com>
2026-03-20 22:45:05 +0530

    fix: add standalone output for Render deployment

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'avatars.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'socialify.git.ci',
       },
     ],
   },
